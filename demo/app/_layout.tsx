3625ca2988b1f56065be8681f335ec16c99145ef
FelipeFadel <dasilvafadelfelipe@gmail.com>
2025-10-08 20:29:32 -0300

    adicionando layout's corretos

--- demo/app/_layout.tsx
+++ demo/app/_layout.tsx
@@ -2,19 +2,15 @@ import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        title: "Voltar",
-        headerStyle: {
-          backgroundColor: "#FFF2E9",
-        },
-        headerTintColor: "#030200",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="home" options={{}} />
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen name="recs" options={{ headerShown: false }} />
     </Stack>
   );
 }
