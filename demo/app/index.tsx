7fa49e28e08cbe094314dbf66b8a267e181d7343
Felipe Fadel <67320543+FelipeFadel@users.noreply.github.com>
2025-10-08 20:05:53 -0300

    @felipefadel/2/router inicial (#5)
    * instalando ReactNativeElements
    
    * organizando expo router
    
    * index principal
    
    * rotas inicias

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,11 +1,42 @@
+import { Button } from "@rneui/themed";
+import { Href, router } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  function getCurrentDate() {
+    return new Date();
+  }
+
+  function openRecs(path: Href) {
+    router.push(path);
+  }
+
+  const today = getCurrentDate();
+  console.log(today);
+
   return (
     <View style={styles.container}>
       <View style={styles.textContainer}>
         <Text style={styles.text}>Rec</Text>
         <Text style={styles.text}>Demo</Text>
+      </View>
+      <View style={styles.flex}>
+        <Button
+          onPress={() => openRecs("/recs/")}
+          title={"Gravar"}
+          containerStyle={{
+            marginHorizontal: 10,
+            marginVertical: 10,
+          }}
+        />
+        <Button
+          onPress={() => openRecs("/recs/teste")}
+          title={"Ver gravações"}
+          containerStyle={{
+            marginHorizontal: 10,
+            marginVertical: 10,
+          }}
+        />
       </View>
     </View>
   );
@@ -14,9 +45,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
+    backgroundColor: "#FFF2E9",
     alignItems: "center",
     justifyContent: "flex-start",
+  },
+  flex: {
+    width: "80%",
   },
   textContainer: {
     alignSelf: "flex-start",
@@ -24,7 +58,7 @@ const styles = StyleSheet.create({
     top: 32,
   },
   text: {
-    color: "#fff",
+    color: "#030200",
     fontSize: 50,
   },
 });
---
+++ demo/app/recs/[data_id].tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Save</Text>
+        <Text style={styles.text}>Demo</Text>
+        <Text style={styles.text}></Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF2E9",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  textContainer: {
+    alignSelf: "flex-start",
+    padding: 32,
+    top: 32,
+  },
+  text: {
+    color: "#030200",
+    fontSize: 50,
+  },
+});
